d3202a3bbd9d8d41fb029ecb173b948c2c431dbb
anton0xf <anton0xf@gmail.com>
2022-12-01 18:57:19 +0300

    comment failed test

--- test/aoc2022/core_test.clj
+++ test/aoc2022/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [aoc2022.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(comment
+  (deftest a-test
+    (testing "FIXME, I fail."
+      (is (= 0 1)))))
