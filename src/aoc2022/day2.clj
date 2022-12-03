a00a9a9e82d006a3d9002e7b2cc8b94afab0e78b
anton0xf <anton0xf@gmail.com>
2022-12-03 09:50:33 +0300

    day2: finish

--- src/aoc2022/day2.clj
+++ src/aoc2022/day2.clj
@@ -23,7 +23,7 @@
 ;; Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock
 ;; If both players choose the same shape, the round instead ends in a draw.
 
-(defn win [x y]
+(defn result [x y]
   (match [x y]
          ["A" "Z"] :lose ["C" "X"] :win
          ["C" "Y"] :lose ["B" "Z"] :win
@@ -31,16 +31,40 @@
          [_ _] :draw))
 
 (defn parse-input [s]
-  (->> (str/split-lines test-guide-input)
+  (->> (str/split-lines s)
        (map #(str/split % #" "))))
 
 (defn score [[x y]]
   (+ (selection-score y)
-     (outcomes (win x y))))
+     (outcomes (result x y))))
 
 (defn total-score [turns]
   (->> turns
        (map score)
+       (reduce +)))
+
+;; part 2
+
+;; the second column says how the round needs to end:
+;; X means you need to lose,
+;; Y means you need to end the round in a draw, and
+;; Z means you need to win
+
+(def expected-result {"X" :lose, "Y" :draw, "Z" :win})
+
+(defn select-shape [x expected-result]
+  (->> (keys selection-score)
+       (filter #(= expected-result (result x %)))
+       (first)))
+
+(defn score2 [[x y]]
+  (let [result (expected-result y)]
+    (+ (selection-score (select-shape x result))
+       (outcomes result))))
+
+(defn total-score2 [turns]
+  (->> turns
+       (map score2)
        (reduce +)))
 
 (comment
@@ -49,8 +73,24 @@
   test-guide ;; => (["A" "Y"] ["B" "X"] ["C" "Z"])
   (map (fn [[x y]]
          {:score (selection-score y)
-          :outcome (outcomes (win x y))})
+          :outcome (outcomes (result x y))})
        test-guide)
   ;; => ({:score 2, :outcome 6} {:score 1, :outcome 0} {:score 3, :outcome 3})
   (total-score test-guide) ;; => 15
+
+  (->> (keys selection-score)
+       (filter #(= :draw (result "A" %)))
+       (first)) ;; => "X"
+  (select-shape "A" :draw) ;; => "X"
+  (let [x "A", y "Y",
+        result (expected-result y)]
+    {:score (selection-score (select-shape x result))
+     :outcome (outcomes result)})
+  ;; => {:score 1, :outcome 3}
+  (score2 ["A" "Y"]) ;; => 4
+  (total-score2 test-guide) ;; => 12 
+
+  (def data (parse-input (slurp (io/resource "day2/input.txt"))))
+  (total-score data) ;; => 15523
+  (total-score2 data) ;; => 15702
   )
