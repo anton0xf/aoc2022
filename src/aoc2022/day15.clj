361433630d049d177f51ea10a3e5c8b010b54e4e
anton0xf <anton0xf@gmail.com>
2022-12-21 14:29:57 +0300

    day15: Beacon Exclusion Zone - part1 - merge intervals

---
+++ src/aoc2022/day15.clj
@@ -0,0 +1,98 @@
+(ns aoc2022.day15
+  (:require [aoc2022.util :refer :all]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as sets]
+            [clojure.core.match :refer [match]]
+            [clojure.math.numeric-tower :as math]))
+
+(defn parse-line [line]
+  (match (str/split line #"[\s=,:]+")
+         ["Sensor" "at" "x" sx "y" sy "closest" "beacon" "is" "at" "x" bx "y" by]
+         (mapv (fn [p] (mapv #(Integer/parseInt %) p))
+               [[sx sy] [bx by]])))
+
+(defn parse-input [in]
+  (->> (line-seq in)
+       (map parse-line)))
+
+(def test-data
+  (with-open [in (io/reader (io/resource "day15/test.txt"))]
+    (vec (parse-input in))))
+
+(defn dist [a b]
+  (abs (- a b)))
+
+
+(defn md "Manhattan distance" [p0 p1]
+  (->> (map dist p0 p1)
+       (apply +)))
+
+;; (md s [x y]) <= (md s b)
+;; (dist sx x) + (dist sy y) <= (md s b) =: r
+;; 0 <= (dist sx x) = |sx - x| <= (md s b) - (dist sy y) =: ry
+;; -ry <= x - sx <= ry
+;; sx - ry <= x <= <= sx + ry
+(defn interval-at-y [y [sx sy :as s] [bx by :as b]]
+  (let [r (md s b)
+        ry (- r (dist sy y))]
+    (if (<= 0 ry)
+      [(- sx ry) (+ sx ry)])))
+
+(defn in-interval? [x [l r]] (<= l x r))
+
+;; inefficient reference implementation
+(defn merge-intervals-count0 [intervals]
+  (let [ls (map first intervals)
+        rs (map second intervals)
+        l-min (reduce min ls)
+        r-max (reduce max rs)]
+    (->> (range l-min (inc r-max))
+         (filter (fn [x] (some #(in-interval? x %) intervals)))
+         count)))
+
+(defn merge-intervals [is]
+  (let [ls (group-by first is)
+        rs (group-by second is)
+        xs (into (sorted-set) (concat (keys ls) (keys rs)))]
+    ;; (println "ls:" ls "rs:" rs "xs:" xs)
+    (loop [xs xs, oc 0, l nil, res []]
+      ;; (println "xs:" xs "oc:" oc "l:" l "res:" res)
+      (assert (<= 0 oc))
+      (assert (or (and (pos? oc) l)
+                  (and (zero? oc) (nil? l))))
+      (if (empty? xs)
+        (do (assert (nil? l) (format "l: %d" l))
+            (assert (zero? oc))
+            res)
+        (let [x (first xs), xs (rest xs)
+              l? (seq (get ls x))
+              r? (seq (get rs x))
+              oc (-> oc
+                     (+ (count (get ls x [])))
+                     (- (count (get rs x []))))
+              l (if l? (or l x) l)]
+          (assert (<= 0 oc))
+          (cond (and r? (zero? oc)) (recur xs oc nil (conj res [l x]))
+                r? (do (assert (pos? oc))
+                       (recur xs oc l res))
+                :else (do (assert (seq (get ls x)) (format "x: %s, ls: %s" x ls))
+                          (recur xs oc l res))))))))
+
+(defn merge-intervals-count [is]
+  (->> (merge-intervals is)
+       (map (fn [[l r]] (inc (- r l))))
+       (reduce +)))
+
+(comment
+  (first test-data)
+  ;; => [[2 18] [-2 15]]
+
+  (->> test-data (keep #(apply interval-at-y 10 %)))
+  ;; => ([12 12] [2 14] [2 2] [-2 2] [16 24] [14 18])
+
+  (->> test-data (keep #(apply interval-at-y 10 %))
+       merge-intervals)
+  ;; => #{-2 2 12 14 16 18 24}
+  )
+
---
+++ test/aoc2022/day15_test.clj
@@ -0,0 +1,75 @@
+(ns aoc2022.day15-test
+  (:require [aoc2022.day15 :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest test-parse-line
+  (is (= [[2 18] [-2 15]]
+         (parse-line "Sensor at x=2, y=18: closest beacon is at x=-2, y=15"))))
+
+(deftest test-md
+  (is (= 1 (md [0 0] [0 1])))
+  (is (= 1 (md [0 0] [0 -1])))
+  (is (= 1 (md [0 0] [1 0])))
+  (is (= 1 (md [0 0] [-1 0])))
+  (is (= 1 (md [0 1] [0 0])))
+  (is (= 1 (md [0 -1] [0 0])))
+  (is (= 1 (md [1 0] [0 0])))
+  (is (= 1 (md [-1 0] [0 0])))
+  (is (= 5 (md [1 1] [3 4]))))
+
+(deftest test-interval-at-y
+  (is (nil? (interval-at-y 10 [2 18] [-2 15])))
+  (is (= [-2 2] (interval-at-y 10 [0 11] [2 10])))
+  (is (= [-4 4] (interval-at-y 3 [0 0] [2 5])))
+  (is (= [-4 4] (interval-at-y -3 [0 0] [2 5])))
+  (is (= [-4 4] (interval-at-y 3 [0 0] [-2 5])))
+  (is (= [-4 4] (interval-at-y 3 [0 0] [2 -5])))
+  (is (= [-3 5] (interval-at-y 3 [1 0] [3 5])))
+  (is (= [-4 4] (interval-at-y 3 [0 0] [5 2])))
+  (is (= [-3 5] (interval-at-y 3 [1 0] [2 6]))))
+
+(deftest test-merge-intervals-count0
+  (is (= 8 (merge-intervals-count0 '([0 0] [2 5] [2 6] [8 9]))))
+  (is (= 4 (merge-intervals-count0 '([2 5] [2 5]))))
+  (is (= 4 (merge-intervals-count0 '([2 5] [2 3]))))
+  (is (= 4 (merge-intervals-count0 '([2 5] [3 5]))))
+  (is (= 4 (merge-intervals-count0 '([2 5] [3 4]))))
+  (is (= 4 (merge-intervals-count0 '([2 5] [2 5] [3 4] [3 5] [2 4]))))
+  (is (= 7 (merge-intervals-count0 '([0 4] [2 6] [3 5]))))
+  (is (every? #(= 7 %)
+              (for [l (range 0 7), r (range l 7)]
+                (merge-intervals-count0 (list [0 4] [2 6] [l r])))))
+  (is (= 27 (->> test-data (keep #(apply interval-at-y 10 %)) merge-intervals-count0))))
+
+(deftest test-merge-intervals
+  (is (= '([0 1]) (merge-intervals '([0 1]))))
+  (is (= '([0 0]) (merge-intervals '([0 0]))))
+  (is (= '([0 2]) (merge-intervals '([0 1] [1 2]))))
+  (is (= '([0 2]) (merge-intervals '([0 2] [1 1]))))
+
+  (is (= '([0 0] [2 6] [8 9]) (merge-intervals '([0 0] [2 5] [2 6] [8 9]))))
+  (is (= '([2 5]) (merge-intervals '([2 5] [2 5]))))
+  (is (= '([2 5]) (merge-intervals '([2 5] [2 3]))))
+  (is (= '([2 5]) (merge-intervals '([2 5] [3 5]))))
+  (is (= '([2 5]) (merge-intervals '([2 5] [3 4]))))
+  (is (= '([2 5]) (merge-intervals '([2 5] [2 5] [3 4] [3 5] [2 4]))))
+  (is (= '([0 6]) (merge-intervals '([0 4] [2 6] [3 5]))))
+  (is (every? #(= '([0 6]) %)
+              (for [l (range 0 7), r (range l 7)]
+                (merge-intervals (list [0 4] [2 6] [l r])))))
+  (is (= '([-2 24]) (->> test-data (keep #(apply interval-at-y 10 %)) merge-intervals))))
+
+(deftest test-merge-intervals-count
+  (is (= 8 (merge-intervals-count '([0 0] [2 5] [2 6] [8 9]))))
+  (is (= 4 (merge-intervals-count '([2 5] [2 5]))))
+  (is (= 4 (merge-intervals-count '([2 5] [2 3]))))
+  (is (= 4 (merge-intervals-count '([2 5] [3 5]))))
+  (is (= 4 (merge-intervals-count '([2 5] [3 4]))))
+  (is (= 4 (merge-intervals-count '([2 5] [2 5] [3 4] [3 5] [2 4]))))
+  (is (= 7 (merge-intervals-count '([0 4] [2 6] [3 5]))))
+  (is (every? #(= 7 %)
+              (for [l (range 0 7), r (range l 7)]
+                (merge-intervals-count (list [0 4] [2 6] [l r])))))
+  (is (= 27 (->> test-data (keep #(apply interval-at-y 10 %)) merge-intervals-count))))
+
+
